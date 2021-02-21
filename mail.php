1a7dfa3a37c9bc60506704f24798e093969989f5
PavanKumar2410 <pavankumark24102000@gmail.com>
2021-02-21 12:57:59 +0530

    Php mailer

--- mail.php
+++ mail.php
@@ -1,13 +1,48 @@
 <?php
-$to = "tejareddy8303@gmail.com";
-$subject = "testing..";
-$message = "testing stage";
-$headers = "From: tejareddy8303@gmail.com";
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-if (mail($to, $subject, $message, $headers)){
-    echo "Mail sent sucessfully";
-    }    else{
-        echo "failed to send mail";
-    }
+//Load Composer's autoloader
+require 'vendor/autoload.php';
 
-?>
+//Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'farmindia58@gmail.com';                     //SMTP username
+    $mail->Password   = 'farmindia123';                               //SMTP password
+    $mail->SMTPSecure = 'tls';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('farmindia58@gmail.com', 'Teja');
+    $mail->addAddress('farmindia58@gmail.com');     //Add a recipient
+    $mail->addAddress('ellen@example.com');               //Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+   // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+   // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $body = "<p><strong>Hello everyone</strong></p>";
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'This is a test email';
+    $mail->Body    = $body;
+    $mail->AltBody = strip_tags($body);
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
