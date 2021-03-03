aee15096c2d13ee9c6f4b9e59d0c5edcc870b0e0
PavanKumar2410 <pavankumark24102000@gmail.com>
2021-03-03 10:43:11 +0530

    UI improvements

--- FarmJobs/admin/navbar.php
+++ FarmJobs/admin/navbar.php
@@ -7,17 +7,31 @@
 		
 		<div class="sidebar-list">
 
-				<a href="index.php?page=home" class="nav-item nav-home"><span class='icon-field'><i class="fa fa-home"></i></span> Home</a>
+				<!--<a href="index.php?page=home" class="nav-item nav-home"><span class='icon-field'><i class="fa fa-home"></i></span> Home</a>
 			
-				<a href="index.php?page=applications" class="nav-item nav-applications"><span class='icon-field'><i class="fa fa-user-tie"></i></span> Applications</a>	-->
+				<a href="index.php?page=applications" class="nav-item nav-applications"><span class='icon-field'><i class="fa fa-user-tie"></i></span> Applications</a>	
 				<a href="index.php?page=vacancy" class="nav-item nav-vacancy"><span class='icon-field'><i class="fa fa-list-alt"></i></span> Vacancy</a>	
 				
-				<a href="index.php?page=recruitment_status" class="nav-item nav-recruitment_status"><span class='icon-field'><i class="fa fa-th-list"></i></span> Status Category</a>		-->
+				<a href="index.php?page=recruitment_status" class="nav-item nav-recruitment_status"><span class='icon-field'><i class="fa fa-th-list"></i></span> Status Category</a>		
 				<?php if($_SESSION['login_type'] == 1): ?>
 				
 				<a href="index.php?page=users" class="nav-item nav-users"><span class='icon-field'><i class="fa fa-users"></i></span> Users</a>
-				<a href="index.php?page=site_settings" class="nav-item nav-site_settings"><span class='icon-field'><i class="fa fa-cogs"></i></span> Settings</a>-->
+				<a href="index.php?page=site_settings" class="nav-item nav-site_settings"><span class='icon-field'><i class="fa fa-cogs"></i></span> Settings</a>
 				<a href="index.php?page=sendmail" class="nav-item nav-site_settings"><span class='icon-field'><i class="bi bi-envelope-fill"></i></span> Send Mail</a>-->
+
+				<div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+    <a href="index.php?page=home" class="nav-link active" data-toggle="" aria-selected="true"><span class='icon-field'><i class="fa fa-home"></i></span>  Home</a>
+    <a href="index.php?page=applications" class="nav-link" data-toggle=""  aria-selected="false"><span class='icon-field'><i class="fa fa-list"></i></span>  Applications</a>
+    <a href="index.php?page=vacancy" class="nav-link" data-toggle="" aria-selected="false"><span class='icon-field'><i class="fa fa-th-list"></i></span>  Vacancy</a>
+    <a href="index.php?page=recruitment_status" class="nav-link" data-toggle="" aria-selected="false"><span class='icon-field'><i class="bi bi-hourglass-split"></i></span>  Status Category</a>
+	<a href="index.php?page=sendmail" class="nav-link" data-toggle="" aria-selected="false"><span class='icon-field'><i class="bi bi-envelope-fill"></i></i></span>  Send Mail</a>
+  </div>
+  <div class="tab-content" id="v-pills-tabContent">
+    <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
+    <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
+    <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
+    <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+  </div>
 				
 			<?php endif; ?>
 		</div>
--- farmvehicle/admin/navbar.php
+++ farmvehicle/admin/navbar.php
@@ -1,5 +1,10 @@
 
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+
 <style>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
 	.collapse a{
 		text-indent:10px;
 	}
@@ -8,10 +13,11 @@
 	}
 </style>
 
+
 <nav id="sidebar" class='mx-lt-5 bg-dark' >
 		
 		<div class="sidebar-list">
-				<a href="index.php?page=home" class="nav-item nav-home"><span class='icon-field'><i class="fa fa-tachometer-alt "></i></span> Dashboard</a>
+				<!--<a href="index.php?page=home" class="nav-item nav-home"><span class='icon-field'><i class="fa fa-tachometer-alt "></i></span> Dashboard</a>
 				<a href="index.php?page=orders" class="nav-item nav-orders"><span class='icon-field'><i class="fa fa-clipboard-list"></i></span> Orders</a>
 				<?php if($_SESSION['login_type'] == 1): ?>
 				<div class="mx-2 text-white">Master List</div>
@@ -23,15 +29,34 @@
 				<?php if($_SESSION['login_type'] == 1): ?>
 				<div class="mx-2 text-white">Systems</div>
 				<a href="index.php?page=users" class="nav-item nav-users"><span class='icon-field'><i class="fa fa-users "></i></span> Users</a>
-				<a href="index.php?page=site_settings" class="nav-item nav-site_settings"><span class='icon-field'><i class="fa fa-cogs"></i></span> System Settings</a>
+				<a href="index.php?page=site_settings" class="nav-item nav-site_settings"><span class='icon-field'><i class="fa fa-cogs"></i></span> System Settings</a>-->
+
+				<div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+    <a href="index.php?page=home" class="nav-link active" data-toggle="" aria-selected="true"><span class='icon-field'><i class="fa fa-home"></i></span>  Home</a>
+    <a href="index.php?page=categories" class="nav-link" data-toggle=""  aria-selected="false"><span class='icon-field'><i class="fa fa-list"></i></span>  Categories</a>
+    <a href="index.php?page=books" class="nav-link" data-toggle="" aria-selected="false"><span class='icon-field'><i class="fa fa-th-list"></i></span>  Equipments</a>
+    <a href="index.php?page=sales_report" class="nav-link" data-toggle="" aria-selected="false"><span class='icon-field'><i class="bi bi-bar-chart-line-fill"></i></span>  Sales Reports</a>
+  </div>
+  <div class="tab-content" id="v-pills-tabContent">
+    <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
+    <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
+    <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
+    <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+  </div>
+
+
+
 			<?php endif; ?>
 		</div>
 
 </nav>
+</body>
 <script>
+
 	$('.nav_collapse').click(function(){
 		console.log($(this).attr('href'))
 		$($(this).attr('href')).collapse()
 	})
 	$('.nav-<?php echo isset($_GET['page']) ? $_GET['page'] : '' ?>').addClass('active')
 </script>
+ 
