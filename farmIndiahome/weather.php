5cccd6d3c2627745293805cd182d78e35c458a39
pavankumar2410 <pavankumark24102000@gmail.com>
2021-03-12 11:16:16 +0530

    12/3/21

--- farmIndiahome/weather.php
+++ farmIndiahome/weather.php
@@ -10,15 +10,9 @@
     <body>    
             
         <div class="container" align="left" style="height:50%;font-weight: 20px ">    
-        
-<<<<<<< HEAD
-               <h1 style="background-color: blackcolor: white;">Weather !!</h1><br>    
-=======
-               <h1 style="background-color: black;color: white;">Weather</h1><br>    
->>>>>>> 6d7b4b603bd3a6af537a3f97d70729d6ac86e208
             <div class="app-title">    
         
-        
+             <h1 style="background-color: black;color: white;">Weather</h1><br>  
                   
             </div>    
             <div class="notification"> </div>    
